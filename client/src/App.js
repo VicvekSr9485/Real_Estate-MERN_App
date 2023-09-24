798232307c9a62e426f683bf7bc05a68201425fd
VicvekSr9485 <olamideoso65@gmail.com>
2023-09-24 03:41:21 +0100

    feats: add necessary routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,40 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div>
-      
+      <Routes>
+        <Route path='/' element={
+          <>
+          <Navbar />
+          <Hero />
+          <PopularProperties />
+          <FeaturedProperties />
+          <Newsletter />
+          <Footer />
+          </>
+        } />
+        
+        <Route path='/properties' element={
+          <>
+          <Navbar />
+          <Properties />
+          <Footer />
+          </>
+        } />
+
+        <Route path='/propertyDetail/:id' element={
+          <>
+          <Navbar />
+          <ProperDetail />
+          <Footer />
+          </>
+        } />
+
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/signin' element={<Signin />} />
+      </Routes>
     </div>
   );
 }
